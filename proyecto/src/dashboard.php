e6c4394a7ab7f3d9ddced61577e3dabd76f2e004
15Cesaro49 <17cesarserrano17@gmail.com>
2024-08-27 18:18:24 -0600

    a

--- proyecto/src/dashboard.php
+++ proyecto/src/dashboard.php
@@ -1,13 +1,19 @@
 <?php
-session_start();
+session_start(); // Inicia o reanuda una sesión existente. 
 
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['user_email'])) {
+    // Si no hay una variable de sesión 'user_email', el usuario no está autenticado.
+    // Redirige al usuario a la página de inicio de sesión.
     header("Location: ../login.html");
-    exit();
+    exit(); // Detiene la ejecución del script después de la redirección.
 }
 
 echo "Bienvenido, " . htmlspecialchars($_SESSION['user_email']) . "!";
+// Muestra un mensaje de bienvenida al usuario autenticado
+// htmlspecialchars() se usa para evitar la inyección de código HTML o JavaScript.
+
 ?>
 
 <a href="PHP/logout.php">Cerrar Sesión</a>
+<!-- Enlace para cerrar sesión, que lleva al usuario a 'logout.php' para terminar la sesión y redirigirlo -->
